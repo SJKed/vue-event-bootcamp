74d772dd7b06e6a598aca8a353c8e2bb85b378d9
SJKed <simonjurstedtskolan@gmail.com>
2022-01-19 00:37:36 +0100

    Not done

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,70 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <h1>Welcome to Your Vue.js App</h1>
+    <button v-on:click="alertButton">Click Me</button>
+    <button v-on:click="counterButton">
+      Click on {{ FirstCounter }} times
+    </button>
+    <button v-on:click="counterButton2">
+      Counting down from {{ SecondCounter }}
+    </button>
+    <button v-on:click="toggle">{{ toggleString }}</button>
+    <p v-on:keypress="diceRoll">{{ diceText }}</p>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      FirstCounter: 0,
+      SecondCounter: 10,
+      toggleString: 'OFF',
+      diceText: 'Press a key to roll the dice',
+      
+    };
+  },
+  methods: {
+    alertButton() {
+      alert('You clicked the button!');
+    },
+    counterButton() {
+      this.FirstCounter++;
+    },
+    counterButton2() {
+      if(this.SecondCounter > 0) {
+        this.SecondCounter--;
+      }
+    },
+    toggle() {
+      if(this.toggleString === 'OFF') {
+        this.toggleString = 'ON';
+      } else {
+        this.toggleString = 'OFF';
+      }
+    },
+    //Press a numkey between 1-9 to roll a dice between it and 0
+    diceRoll(e) {
+      e = e.KeyCode;
+      if(e.key >= 49 && e.key <= 57) {
+        this.diceText = 'You rolled a ' + e.key + '!';
+      } else {
+        this.diceText = 'Press a numkey between 1-9 to roll a dice between it and 0';
+      }
+    },
+  },
+};
+</script>
+
+<style>
+div {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  border: 1px solid black;
+  padding: 2rem;
+  margin-left: auto;
+  margin-right: auto;
+  width: 40%;
+}
+</style>
